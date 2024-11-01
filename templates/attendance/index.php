3b41842cea8882001c90213847c6aa8a201b3cd7
Vlad Isupov <vlad@CT106.10.171.72.3>
2024-11-01 04:35:23 +0000

    base styles

---
+++ templates/attendance/index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Добавить посещаемость</title>
+    <link rel="stylesheet" href="/report/static/css/attendance.css">
+</head>
+
+<body>
+
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/report/templates/header.php"; ?>
+
+    
+
+    <?php require $_SERVER['DOCUMENT_ROOT'] . "/report/templates/footer.php"; ?>
+
+</body>
+
+</html>
---
+++ templates/header.php
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="/report/static/css/style.css">
+
+<header>
+    <div class="header_logo">Report</div>
+    <div class="header_options">
+        <a href="/report/attendance/index" class="header_options__item hover-underline-animation">Добавить</a>
+        <a href="/report/statistic/index" class="header_options__item hover-underline-animation">Статистика</a>
+    </div>
+</header>
