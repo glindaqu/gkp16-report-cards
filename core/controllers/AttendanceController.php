2e1845507a23b2087b462bbd992f7afb932c2bef
Vlad Isupov <vlad@CT106.10.171.72.3>
2024-11-02 07:22:57 +0000

    attendance adding

--- core/controllers/AttendanceController.php
+++ core/controllers/AttendanceController.php
@@ -3,16 +3,20 @@
 require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/views/AttendanceView.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/Controller.php";
 require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/models/EmployeeModel.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/models/AttendanceModel.php";
 
 class AttendanceController extends Controller
 {
     private AttendanceView $view;
     private EmployeeModel $employee_model;
+    private AttendanceModel $attendance_model;
+
 
     public function __construct()
     {
         $this->view = new AttendanceView();
         $this->employee_model = new EmployeeModel();
+        $this->attendance_model = new AttendanceModel();
     }
 
     public function index(): void
@@ -22,6 +26,15 @@ class AttendanceController extends Controller
 
     public function add(): void
     {
-        //TODO: сделать добавление посещаемости
+        $income_time = $_POST['income'];
+        $outcome_time = $_POST['outcome'];
+        $employee_id = $_POST['employee_id'];
+
+        $date = date("Y-m-d");
+
+        $income = DateTime::createFromFormat("Y-m-d H:i", "$date $income_time");
+        $outcome = DateTime::createFromFormat("Y-m-d H:i", "$date $outcome_time");
+
+        $this->attendance_model->add($employee_id, $income, $outcome);
     }
 }
---
+++ core/models/AttendanceModel.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/Model.php";
+require_once $_SERVER['DOCUMENT_ROOT'] . "/report/core/Database.php";
+
+class AttendanceModel extends Model {
+
+    private Database $db;
+
+    public function __construct() {
+        $this->db = new Database();
+    }
+
+    public function add(int $employee_id, DateTime $in, DateTime $out): void {
+        $this->db->insert_attendance($in, $out, $employee_id);
+    }
+
+    public function get_all(): array {
+        return $this->db->get_attendance();
+    }
+
+    public function get_by_employee(int $id): array {
+        return $this->db->get_attendance_by_employee($id);
+    }
+
+}
