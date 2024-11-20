c8db25c8d97234757a6cdec10f1366f05f552867
glindaqu <q5481996@gmail.com>
2024-11-20 11:39:03 +0700

    header update

--- templates/header.php
+++ templates/header.php
@@ -1,10 +1,15 @@
 <link rel="stylesheet" href="/report/static/css/style.css">
 
 <header>
-    <div class="header_logo">Report</div>
+    <a href="/report/" class="header_logo">
+        Report
+    </a>
     <div class="header_group">
-        <a href="/report/authorize/logout"
-            class="header_options__item hover-underline-animation"><?= $_COOKIE['user_name'] ?></a>
-        <a href="/report/authorize/logout" class="header_options__item hover-underline-animation">Выйти</a>
+        <div class="header_options__item"><?= $_COOKIE['user_name'] ?></div>
+        <a href="/report/authorize/logout" class="header_options__item">
+            <button class="logout">
+                Выйти
+            </button>
+        </a>
     </div>
 </header>
